76909cac2e7d9f461f3b6e13bff79867e9f2c4cd
Nicole Freed <nicole.freed@gmail.com>
2017-07-07 13:19:59 -0700

    create app route /display-count and form handler

--- app/app.php
+++ app/app.php
@@ -10,6 +10,15 @@
     $app->get('/', function() use($app) {
         return $app['twig']->render('input_form.html.twig');
     });
-    
+
+    $app->post('/display-count', function() use($app) {
+        $word = $_POST['word'];
+        $sentence = $_POST['sentence'];
+        $new_repeat_counter = new RepeatCounter($word, $sentence);
+        $count = $new_repeat_counter->wordCount($word, $sentence);
+
+        return $app['twig']->render('results.html.twig', array('display_word' => $word, 'display_sentence' => $sentence, 'display_count' => $count));
+    });
+
     return $app;
 ?>
