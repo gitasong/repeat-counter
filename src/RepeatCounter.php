c6c180641e2e61617d70b3034029a7f4d274eb49
Nicole Freed <nicole.freed@gmail.com>
2017-07-08 12:09:20 -0700

    pass test for adjacent non-alpha/non-whitespace characters

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -2,10 +2,28 @@
 
     class RepeatCounter
     {
+        // function countRepeats($word_input, $sentence_input)
+        // {
+        //     $count = 0;
+        //     $sentence_arr = explode(" ", $sentence_input);
+        //     foreach ($sentence_arr as $item) {
+        //         if ($word_input == $item) {
+        //             $count++;
+        //         }
+        //     }
+        //     return $count;
+        // }
+
         function countRepeats($word_input, $sentence_input)
         {
+            $stripped_sentence = "";
             $count = 0;
-            $sentence_arr = explode(" ", $sentence_input);
+            for ($i = 0; $i < strlen($sentence_input); $i++) {
+                if (ctype_alpha($sentence_input[$i]) || (ctype_space($sentence_input[$i]))) {
+                    $stripped_sentence = $stripped_sentence . $sentence_input[$i];
+                }
+            }
+            $sentence_arr = explode(" ", $stripped_sentence);
             foreach ($sentence_arr as $item) {
                 if ($word_input == $item) {
                     $count++;
